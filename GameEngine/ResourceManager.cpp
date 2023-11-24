fe18300d3abc1363853e373aeac1e7d6462bf380
ThomasMouranchon <91051862+ThomasMouranchon@users.noreply.github.com>
2023-11-24 12:48:43 +0100

    Adjustement to make it work

--- GameEngine/ResourceManager.cpp
+++ GameEngine/ResourceManager.cpp
@@ -3,8 +3,8 @@
 //sf::Texture* GE::ResourceManager::getTexture(sf::Texture* texture)
 void GE::ResourceManager::getTexture(std::string name)
 {
-	sf::Texture* texture = texture->loadFromFile(name);
+	/*sf::Texture* texture = texture->loadFromFile(name);
 	listTexture.push_back(texture);
 	name: listTexture.size() - 1;
-	return name;
+	return name;*/
 }
